dc8f57a44e2802d5ec5588134eb4fa1f7ed3a7b7
royckl0163 <60773061+royckl0163@users.noreply.github.com>
2022-11-21 17:02:45 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+let topButton = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
